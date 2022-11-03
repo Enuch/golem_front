211321d14868f083b695db38c7d8cfb2f0632a8f
Enuch <enuchsantys@gmail.com>
2022-11-03 15:40:05 -0300

    react-router configurando rotas filhas e chamando todas as rotas filhas dentro da rota raiz

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,16 +1,24 @@
 import {
   createBrowserRouter,
 } from "react-router-dom";
+import { Category } from "../pages/category/Category";
 import { Home } from "../pages/home/Home";
 import { Login } from "../pages/login/Login";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/login",
     element: <Login />,
   },
   {
-    path: "/home",
+    path: "/",
     element: <Home />,
+    children: [
+      {
+        path: "/category",
+        element: <Category />,
+      },
+    ]
   },
+
 ]);
